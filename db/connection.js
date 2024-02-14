618f5ca12d1daa087329d4aee5e086e7691c5f56
EMAD2274 <160025473+EMAD2274@users.noreply.github.com>
2024-02-14 16:04:27 +0200

    Update connection.js

--- db/connection.js
+++ db/connection.js
@@ -4,7 +4,7 @@ import mongoose from "mongoose";
 
 export function connection() {
     mongoose
-      .connect("mongodb+srv://usefemad:1QWE23456789qwe@7mo.z4prwhk.mongodb.net/metro")
+      .connect("mongodb+srv://usefemad:1QWE23456789qwe@7mo.z4prwhk.mongodb.net")
       .then(() => console.log("db connected"))
       .catch((err) => console.log("db err"));
 
